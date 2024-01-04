6a617079c350285b4b2b3e33d4e1f7fcda5578c5
kertelizerkat <114565637+kertelizerkat@users.noreply.github.com>
2024-01-04 15:32:42 +0200

    Add files via upload

---
+++ project2/system.config/c.cmd
@@ -0,0 +1,123 @@
+import os
+print('Notice!! you can only decrypt a file if only you have encrypted it using the same my method !')
+print('')
+filename= str(input('Enter the file-path to encrypt or decrypt your file :'))
+analyst=str(input('Are you in encryting(E) or decrypting(D) E/D :'))
+if analyst=='d' or analyst=='D':
+    extention=input('enter your file extention:')
+if not os.path.exists('collector2'):
+    os.mkdir('collector2')
+if  os.path.exists(filename):
+    with open(filename, 'r') as file:
+        if analyst=='E' or analyst=='e' :
+            text= file.read()
+            textbine=' '.join(format(ord(c), 'b') for c in text)
+            crypt=textbine
+            try:
+                r=str(crypt)
+                re=len(r)-1
+                time=re/240
+                time=int(time)
+                minutes=time/60
+                minutes=int(minutes)
+                print(f'there are {re} binary characters')
+                print('')
+                print(f'Estimated time to complete is {time} seconds which is equivalent to {minutes} minutes')
+                print('')
+                storer=''
+                while re>=0:
+                    storer= storer + r[re]
+                    print(f'{re} characters left')
+                    re=re-1
+                print('_____________________________________________________________')
+                print('')
+                print(f'{storer}')
+                print('')
+                print('______________________________________________________________')
+                print('')
+                print('stage 1 Encryption complete!')
+            except:
+                print('could not encrypt!')
+            extention=str(input('Enter the extention of your new file :'))
+            if not os.path.exists(f'collector2/{filename}(decryption)'):
+                os.mkdir(f'collector2/{filename}(decryption)')
+            try:
+                os.chdir(f'collector2/{filename}(decryption)')
+            except:
+                print('directory could not be changed')
+            with open(f'{filename}2(decryption){extention}', 'w') as file2:
+                file2.write(storer)
+            try:
+                os.chdir(r'C:\project2')
+            except:
+                print('directory could not be changed')
+            try:
+                crypt2=str(storer)
+                crypt2txt="".join(chr(int(c, 2)) for c in crypt2.split(" "))
+            except:
+                print('could not encrypt!')
+        
+            with open(filename, 'w') as f:
+                f.write(crypt2txt)
+                print('')
+                print('stage 2 Encryption complete!')
+        elif analyst=='D' or analyst=='d':
+            if not os.path.exists(f'collector2/{filename}(decryption)'):
+                print('could not find decryption code!!')
+            try:
+                os.chdir(f'collector2/{filename}(decryption)')
+            except:
+                print('directory could not be changed')
+            if os.path.exists(f'{filename}2(decryption){extention}'):
+                with open(f'{filename}2(decryption){extention}', 'r') as file2:
+                    decrypt=file2.read()
+                    try:
+                        r=str(decrypt)
+                        re=len(r)-1
+                        time=re/240
+                        time=int(time)
+                        minutes=time/60
+                        minutes=int(minutes)
+                        print(f'there are {re} binary characters')
+                        print('')
+                        print(f'Estimated time to complete is {time} seconds which is equivalent to {minutes} minutes')
+                        print('')
+                        destorer=''
+                        while re>=0:
+                            destorer= destorer + r[re]
+                            print(f'{re} characters left')
+                            re=re-1
+                        print('_____________________________________________________________')
+                        print('')
+                        print(f'{destorer}')
+                        print('')
+                        print('______________________________________________________________')
+                        print('')
+                        print('stage 1 Decryption complete!')
+                    except:
+                        print('could not decrypt!')
+                    try:
+                        decrypt2=str(destorer)
+                        decrypt2txt="".join(chr(int(c, 2)) for c in decrypt2.split(" "))
+                    except:
+                        print('could not decrypt')
+
+                    with open((r'C:/project2/'+ filename), 'w') as f:
+                        try:
+                            f.write(decrypt2txt)
+                            print('_____________________________________________________________')
+                            print('')
+                            print(f'{decrypt2txt}')
+                            print('______________________________________________________________')
+                            print('')
+                            print('stage 2 Decryption complete!')
+                        except:
+                            print('could not decrypt!')
+            try:
+                os.chdir(r'C:\project2')
+            except:
+                print('directory could not be changed')
+else:
+    print('file does not exist!')
+                    
+input('')    
---
+++ project2/system.config/config.py
@@ -0,0 +1,78 @@
+import os
+
+if not os.path.exists('system.config'):
+    print('could\'nt find system.config')
+    print('')
+    print('Sorry, cannot continue')
+
+if os.path.exists('system.config'):
+    print('')
+    #unexecuted fast copying method
+    def copy_files():
+        for i in ['c','d','e','f','g','h','i','j','k']:
+            with open('encrypt.py','r') as cf:
+                copy= cf.read()
+            with open(f'system.config/{i}.cmd', 'w') as fi:
+                fi.write(copy)
+    #end of method
+    print('Input a Drive Letter from C-K')
+    print('')
+    drive_letter=input('Enter your default drive letter: ')
+    print('')
+    if drive_letter=='C' or drive_letter=='c':
+        drive_letter='c'
+    elif drive_letter=='D' or drive_letter=='d':
+        drive_letter='d'
+    elif drive_letter=='e' or drive_letter=='E':
+        drive_letter='e'
+    elif drive_letter=='f' or drive_letter=='F':
+        drive_letter='f'
+    elif drive_letter=='g' or drive_letter=='G':
+        drive_letter='g'
+    elif drive_letter=='h' or drive_letter=='H':
+        drive_letter='h'
+    elif drive_letter=='i' or drive_letter=='I':
+        drive_letter='i'
+    elif drive_letter=='j' or drive_letter=='J':
+        drive_letter='j'
+    elif drive_letter=='k' or drive_letter=='K':
+        drive_letter='k'
+    while drive_letter!='c' and drive_letter!='d' and drive_letter!='e' and drive_letter!='f' and drive_letter!='g' and drive_letter!='h' and drive_letter!='i' and drive_letter!='j' and drive_letter!='k':
+        print(f'({drive_letter}) !was not identified in a range of C-K!')
+        print('')
+        print('please enter a letter within a range of C-K only!')
+        print('')
+        drive_letter=input('Enter your default drive letter: ')
+        print('')
+        if drive_letter=='C' or drive_letter=='c':
+            drive_letter='c'
+        elif drive_letter=='D' or drive_letter=='d':
+            drive_letter='d'
+        elif drive_letter=='e' or drive_letter=='E':
+            drive_letter='e'
+        elif drive_letter=='f' or drive_letter=='F':
+            drive_letter='f'
+        elif drive_letter=='g' or drive_letter=='G':
+            drive_letter='g'
+        elif drive_letter=='h' or drive_letter=='H':
+            drive_letter='h'
+        elif drive_letter=='i' or drive_letter=='I':
+            drive_letter='i'
+        elif drive_letter=='j' or drive_letter=='J':
+            drive_letter='j'
+        elif drive_letter=='k' or drive_letter=='K':
+            drive_letter='k'
+        
+    with open(f'system.config/{drive_letter}.cmd', 'r')as nav:
+        configured_drive_letter= nav.read()
+        with open('encrypt.py', 'w') as over_write:
+            over_write.write(configured_drive_letter)
+            print('')
+            print('Configuration is successfully completed!')
+            
+else:
+    print('')
+    print('Error message something went wrong!')
+    input('')
+
+    
